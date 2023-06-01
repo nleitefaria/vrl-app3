9c2aead5c72d1bb53df1391996139eaae168e280
nleitefaria <nleitefaria@gmail.com>
2023-06-01 09:34:56 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+  	<div className="h-56 grid grid-cols-6 gap-10 content-center">
+    <div className="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-xl">
+    <h1 className="text-3xl font-bold">
+      Hi my name is prince and i am funky
+    </h1>
+    </div>
+    
+    
+    <div className="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-xl">
+    <h1 className="text-3xl font-bold">
+      Hi my name is prince and i am funky lalalalalalalalalalal
+    </h1>
+    </div>
+    
+    <div className="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-xl">
+    <h1 className="text-3xl font-bold">
+      Hi my name is prince and i am funky lalalalalalalalalalal
+    </h1>
+    </div>
+    
+   </div>
+  );
+}
+
+export default App;
