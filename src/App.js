0d4d933d736bc340fc732d59d9f313bfbec2cad0
nleitefaria <nleitefaria@gmail.com>
2023-06-01 09:53:58 +0100

    INIT

--- src/App.js
+++ src/App.js
@@ -4,25 +4,11 @@ import './App.css';
 function App() {
   return (
   	<div className="h-56 grid grid-cols-6 gap-10 content-center">
-    <div className="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-xl">
-    <h1 className="text-3xl font-bold">
-      Hi my name is prince and i am funky
-    </h1>
-    </div>
-    
-    
-    <div className="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-xl">
-    <h1 className="text-3xl font-bold">
-      Hi my name is prince and i am funky lalalalalalalalalalal
-    </h1>
-    </div>
-    
-    <div className="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-xl">
-    <h1 className="text-3xl font-bold">
-      Hi my name is prince and i am funky lalalalalalalalalalal
-    </h1>
-    </div>
-    
+        <div className="max-w-sm mx-auto p-6 bg-white rounded-lg shadow-xl">
+            <h1 className="text-3xl font-bold">
+                Hi
+            </h1>
+        </div>
    </div>
   );
 }
